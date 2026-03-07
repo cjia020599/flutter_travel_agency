92f4d65aa298fc83b4d8a8ee2733e49dad425bb3
cjia020599 <107036105+cjia020599@users.noreply.github.com>
2026-03-08 01:03:03 +0800

    Create render_build.sh

---
+++ render_build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# 1. Download Flutter
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable
+fi
+
+# 2. Set the Path
+export PATH="$PWD/flutter/bin:$PATH"
+
+# 3. Build the web app
+flutter build web --release
