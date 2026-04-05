d4ffc4bcf53037409d179de4dc6091d36e697a27
cjia020599 <107036105+cjia020599@users.noreply.github.com>
2026-04-05 09:11:27 +0800

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# 1. Download Flutter stable
+# 1. Clone Flutter
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable
 fi
 
-# 2. Add Flutter to the PATH
+# 2. Set Path
 export PATH="$PWD/flutter/bin:$PATH"
 
-# 3. Initialize Flutter for Web (IMPORTANT: This prevents Status 64)
-flutter config --enable-web --no-analytics
+# 3. Setup Environment
+flutter config --no-analytics
+flutter config --enable-web
 flutter precache --web
 
-# 4. Resolve dependencies
-# We run 'pub get' after 'precache' to make sure the packages see the web SDK
+# 4. Refresh Dependencies
+# This clears out any conflicting lock files from local builds
+rm -f pubspec.lock
 flutter pub get
 
-# 5. Build the Web App
-# We use 'canvaskit' for your Map features
-flutter build web --release --dart-define=FLUTTER_WEB_CANVASKIT_URL=https://unpkg.com/canvaskit-wasm@0.37.1/bin/ --no-wasm
+# 5. The Build (The Fix is here)
+# We add --no-wasm to stop the 'wasm dry run' crash
+flutter build web --release --no-wasm
