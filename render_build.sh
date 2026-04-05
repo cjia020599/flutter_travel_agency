a340dfd897aca499577a2e2c8602ea7ebdb1c872
cjia020599 <107036105+cjia020599@users.noreply.github.com>
2026-04-05 09:24:54 +0800

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 1. Clone Flutter
+# 1. Clone a SPECIFIC stable version to avoid the WASM bug
 if [ ! -d "flutter" ]; then
-  git clone https://github.com/flutter/flutter.git -b stable
+  git clone https://github.com/flutter/flutter.git -b 3.22.2
 fi
 
-# 2. Set Path
+# 2. Add to PATH
 export PATH="$PWD/flutter/bin:$PATH"
 
-# 3. Setup (This fixes the Status 64 errors we had)
+# 3. Setup
 flutter config --no-analytics
 flutter config --enable-web
 flutter precache --web
 
-# 4. CRITICAL: Deep Clean (This fixes the Status 1 errors)
-# Removing these forces Render to download Linux-compatible map libraries
+# 4. Clean and Resolve
 rm -rf .dart_tool
 rm -f pubspec.lock
 flutter pub get
 
 # 5. Build
-# We use the basic build command first to ensure it passes
-flutter build web --release
+# This version doesn't have the "Negate no-wasm" error or the dry-run crash
+flutter build web --release --web-renderer canvaskit
