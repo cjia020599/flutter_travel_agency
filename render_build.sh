77961bc63ce9de3c7176d9c3f1e492f916078932
cjia020599 <107036105+cjia020599@users.noreply.github.com>
2026-04-05 09:44:23 +0800

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
-# 1. Force Linux line endings (This fixes the hidden \r character)
-# This is likely why --web-renderer keeps failing
-sed -i 's/\r$//' render_build.sh
+# 1. Download Flutter
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable
+fi
 
-# 2. Fresh Flutter Clone
-rm -rf flutter
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
+# 2. Set the Path
 export PATH="$PWD/flutter/bin:$PATH"
 
-# 3. Setup
-flutter config --no-analytics
-flutter config --enable-web
-flutter precache --web
-
-# 4. Build
-# Try building WITHOUT the renderer flag first to see if it passes
-# If this works, then we can add the flag back
+# 3. Build the web app
 flutter build web --release
