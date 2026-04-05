d25ee7c6210f8fd70266e4a258823d58d735ee1b
cjia020599 <107036105+cjia020599@users.noreply.github.com>
2026-04-05 08:34:40 +0800

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -2,7 +2,7 @@
 # Exit on error
 set -o errexit
 
-# 1. Download Flutter
+# 1. Download Flutter (if not present)
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable
 fi
@@ -10,5 +10,14 @@ fi
 # 2. Set the Path
 export PATH="$PWD/flutter/bin:$PATH"
 
-# 3. Build the web app
-flutter build web --release
+# 3. Upgrade/Verify Flutter 
+# This ensures the tool is downloaded and ready
+flutter doctor -v
+
+# 4. Get dependencies 
+# This is usually why builds fail (missing packages like flutter_map)
+flutter pub get
+
+# 5. Build the web app
+# Added --web-renderer canvaskit as maps often need better performance
+flutter build web --release --web-renderer canvaskit
