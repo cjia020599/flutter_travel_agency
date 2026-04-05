f04f83e5f1e482a54dce326ff99583765837ed1b
cjia020599 <107036105+cjia020599@users.noreply.github.com>
2026-04-05 09:35:57 +0800

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 1. Start fresh - delete old flutter folder if it exists
+# 1. Force Linux line endings (This fixes the hidden \r character)
+# This is likely why --web-renderer keeps failing
+sed -i 's/\r$//' render_build.sh
+
+# 2. Fresh Flutter Clone
 rm -rf flutter
-
-# 2. Clone fresh stable (shallow clone for speed)
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
-
-# 3. Set the Path
 export PATH="$PWD/flutter/bin:$PATH"
 
-# 4. UNLOCK WEB FEATURES (This stops Status 64)
-# We must do these in this EXACT order
+# 3. Setup
+flutter config --no-analytics
 flutter config --enable-web
 flutter precache --web
 
-# 5. Get dependencies for your Travel Agency app
-# We'll remove the lock file to avoid Windows vs Linux conflicts
-rm -f pubspec.lock
-flutter pub get
-
-# 6. The Build
-# If this still gives Error 64, remove "--web-renderer canvaskit"
-# and just use "flutter build web --release" to get it live first.
-flutter build web --release --web-renderer canvaskit
+# 4. Build
+# Try building WITHOUT the renderer flag first to see if it passes
+# If this works, then we can add the flag back
+flutter build web --release
